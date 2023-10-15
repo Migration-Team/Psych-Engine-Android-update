98db839fab9ed4061b9137337b5fd4ecd2ccc51b
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-10-15 20:30:20 +0800

    Update ClientPrefs.hx

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -44,6 +44,7 @@ class SaveVariables {
 	public var hitsoundVolume:Float = 0;
 	public var pauseMusic:String = 'Tea Time';
 	public var checkForUpdates:Bool = true;
+	public var disableNoteRGB:Bool = true;
 	public var comboStacking:Bool = true;
 	public var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
